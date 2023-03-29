032efb583acaa143186a7cf82e020f7f3b629b0c
wieceslaw <vyache-nov@mail.ru>
2023-03-29 18:12:47 +0300

    impl track & untrack commands

--- bot/src/main/java/ru/tinkoff/edu/java/bot/telegram/command/TrackCommand.java
+++ bot/src/main/java/ru/tinkoff/edu/java/bot/telegram/command/TrackCommand.java
@@ -2,21 +2,28 @@ package ru.tinkoff.edu.java.bot.telegram.command;
 
 import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import ru.tinkoff.edu.java.bot.service.ScrapperWebService;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 @Order(4)
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class TrackCommand implements PublicCommand {
     private final ScrapperWebService webService;
 
+    private static final Pattern pattern = Pattern.compile("^\\s*/track (\\S+)\\s*$");
     private static final String COMMAND = "/track";
     private static final String DESCRIPTION = "start tracking link";
     private static final String SUCCESS_RESPONSE = "Added link to your tacking list";
+    private static final String WRONG_FORMAT_RESPONSE = "Use correct format: '\\track <link>'";
 
     @Override
     public String command() {
@@ -30,7 +37,14 @@ public class TrackCommand implements PublicCommand {
 
     @Override
     public SendMessage handle(@NotNull Message message) {
-        // TODO: implement (check pattern /track <link>, then get link, then send to service)
+        String text = message.getText();
+        Matcher matcher = pattern.matcher(text);
+        if (!matcher.matches()) {
+            return new SendMessage(message.getChatId().toString(), WRONG_FORMAT_RESPONSE);
+        }
+        String url = matcher.group(1);
+        log.info("Created link {}", url);
+        webService.createLink(message.getChatId(), url);
         return new SendMessage(message.getChatId().toString(), SUCCESS_RESPONSE);
     }
 
--- bot/src/main/java/ru/tinkoff/edu/java/bot/telegram/command/UntrackCommand.java
+++ bot/src/main/java/ru/tinkoff/edu/java/bot/telegram/command/UntrackCommand.java
@@ -2,21 +2,28 @@ package ru.tinkoff.edu.java.bot.telegram.command;
 
 import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import ru.tinkoff.edu.java.bot.service.ScrapperWebService;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 @Order(5)
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class UntrackCommand implements PublicCommand {
     private final ScrapperWebService webService;
 
+    private static final Pattern pattern = Pattern.compile("^\\s*/untrack (\\S+)\\s*$");
     private static final String COMMAND = "/untrack";
     private static final String DESCRIPTION = "stop tracking link";
     private static final String SUCCESS_RESPONSE = "Removed link from your tacking list";
+    private static final String WRONG_FORMAT_RESPONSE = "Use correct format: '\\untrack <link>'";
 
     @Override
     public String command() {
@@ -30,7 +37,14 @@ public class UntrackCommand implements PublicCommand {
 
     @Override
     public SendMessage handle(@NotNull Message message) {
-        // TODO: implement (check pattern /track <link>, then get link, then send to service)
+        String text = message.getText();
+        Matcher matcher = pattern.matcher(text);
+        if (!matcher.matches()) {
+            return new SendMessage(message.getChatId().toString(), WRONG_FORMAT_RESPONSE);
+        }
+        String url = matcher.group(1);
+        log.info("Removed link {}", url);
+        webService.createLink(message.getChatId(), url);
         return new SendMessage(message.getChatId().toString(), SUCCESS_RESPONSE);
     }
 
