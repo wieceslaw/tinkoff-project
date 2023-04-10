36c8e16d05e84f85311547394f06f2b252680703
wieceslaw <vyache-nov@mail.ru>
2023-04-10 16:30:59 +0300

    added subscription repository tests

--- scrapper/src/test/java/ru/tinkoff/edu/java/scrapper/repository/JdbcSubscriptionRepositoryTest.java
+++ scrapper/src/test/java/ru/tinkoff/edu/java/scrapper/repository/JdbcSubscriptionRepositoryTest.java
@@ -3,14 +3,23 @@ package ru.tinkoff.edu.java.scrapper.repository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
+import ru.tinkoff.edu.java.scrapper.IntegrationEnvironment;
+import ru.tinkoff.edu.java.scrapper.dto.entity.SubscriptionEntity;
+
+import java.sql.PreparedStatement;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
-class JdbcSubscriptionRepositoryTest {
+class JdbcSubscriptionRepositoryTest extends IntegrationEnvironment {
     @Autowired
     private JdbcTemplate template;
 
@@ -22,10 +31,18 @@ class JdbcSubscriptionRepositoryTest {
     @Rollback
     void add__addOne_oneAdded() {
         // given
+        String url = "https://github.com/Wieceslaw/tinkoff-project";
+        Long chatId = 1L;
+        Long linkId = createLink(url);
+        createChat(chatId);
 
         // when
+        int beforeCount = getAll().size();
+        subscriptionRepository.add(chatId, linkId);
+        int afterCount = getAll().size();
 
         // then
+        assertEquals(beforeCount + 1, afterCount);
     }
 
     @Test
@@ -33,10 +50,16 @@ class JdbcSubscriptionRepositoryTest {
     @Rollback
     void add__alreadyExist_throwsException() {
         // given
+        String url = "https://github.com/Wieceslaw/tinkoff-project";
+        Long chatId = 1L;
+        Long linkId = createLink(url);
+        createChat(chatId);
 
         // when
+        subscriptionRepository.add(chatId, linkId);
 
         // then
+        assertThrows(DuplicateKeyException.class, () -> subscriptionRepository.add(chatId, linkId));
     }
 
     @Test
@@ -46,8 +69,12 @@ class JdbcSubscriptionRepositoryTest {
         // given
 
         // when
+        int querySize = getAll().size();
+        List<SubscriptionEntity> all = subscriptionRepository.findAll();
 
         // then
+        assertEquals(all.size(), 0);
+        assertEquals(all.size(), querySize);
     }
 
     @Test
@@ -55,10 +82,19 @@ class JdbcSubscriptionRepositoryTest {
     @Rollback
     void findAll__oneExists_oneReturned() {
         // given
+        String url = "https://github.com/Wieceslaw/tinkoff-project";
+        Long chatId = 1L;
+        Long linkId = createLink(url);
+        createChat(chatId);
+        createSubscription(chatId, linkId);
 
         // when
+        int querySize = getAll().size();
+        List<SubscriptionEntity> all = subscriptionRepository.findAll();
 
         // then
+        assertEquals(all.size(), 1);
+        assertEquals(all.size(), querySize);
     }
 
     @Test
@@ -66,10 +102,20 @@ class JdbcSubscriptionRepositoryTest {
     @Rollback
     void remove__oneExists_oneRemoved() {
         // given
+        String url = "https://github.com/Wieceslaw/tinkoff-project";
+        Long chatId = 1L;
+        Long linkId = createLink(url);
+        createChat(chatId);
+        createSubscription(chatId, linkId);
 
         // when
+        int beforeCount = getAll().size();
+        int removed = subscriptionRepository.remove(chatId, linkId);
+        int afterCount = getAll().size();
 
         // then
+        assertEquals(beforeCount - 1, afterCount);
+        assertEquals(removed, 1);
     }
 
     @Test
@@ -77,10 +123,17 @@ class JdbcSubscriptionRepositoryTest {
     @Rollback
     void remove__notExists_zeroRemoved() {
         // given
+        Long chatId = 1L;
+        Long linkId = 1L;
 
         // when
+        int beforeCount = getAll().size();
+        int removed = subscriptionRepository.remove(chatId, linkId);
+        int afterCount = getAll().size();
 
         // then
+        assertEquals(beforeCount, afterCount);
+        assertEquals(removed, 0);
     }
 
     @Test
@@ -88,10 +141,17 @@ class JdbcSubscriptionRepositoryTest {
     @Rollback
     void countSubscriptions__oneSubscriber_oneCounted() {
         // given
+        String url = "https://github.com/Wieceslaw/tinkoff-project";
+        Long chatId = 1L;
+        Long linkId = createLink(url);
+        createChat(chatId);
+        createSubscription(chatId, linkId);
 
         // when
+        Integer counted = subscriptionRepository.countSubscriptions(linkId);
 
         // then
+        assertEquals(counted, 1);
     }
 
     @Test
@@ -99,9 +159,37 @@ class JdbcSubscriptionRepositoryTest {
     @Rollback
     void countSubscriptions__zeroSubscribers_zeroCounted() {
         // given
+        String url = "https://github.com/Wieceslaw/tinkoff-project";
+        Long chatId = 1L;
+        Long linkId = createLink(url);
+        createChat(chatId);
 
         // when
+        Integer counted = subscriptionRepository.countSubscriptions(linkId);
 
         // then
+        assertEquals(counted, 0);
+    }
+
+    private List<SubscriptionEntity> getAll() {
+        return template.query("select chat_id, link_id from subscription", new BeanPropertyRowMapper<>(SubscriptionEntity.class));
+    }
+
+    private Long createLink(String url) {
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        template.update(con -> {
+            PreparedStatement ps = con.prepareStatement("insert into link (url) values (?)", new String[] {"id"});
+            ps.setString(1, url);
+            return ps;
+        }, keyHolder);
+        return keyHolder.getKey().longValue();
+    }
+
+    private void createChat(Long id) {
+        template.update("insert into chat (id) values (?)", id);
+    }
+
+    private void createSubscription(Long chatId, Long linkId) {
+        template.update("insert into subscription (chat_id, link_id) values (?, ?)", chatId, linkId);
     }
 }
